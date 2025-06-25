c1a1c51e49ac46cf6375fe13f8289443ce9d49a6
darkus079 <165654807+darkus079@users.noreply.github.com>
2025-06-25 21:22:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_data(filename: str):
         return list(reader)
 
 
-def parse_condition(condition: str) -> tuple[str]:
+def parse_filter_condition(condition: str) -> tuple[str]:
     operation_list = ['=', '>=', '<=', '>', '<']
 
     for operation in operation_list:
@@ -35,14 +35,19 @@ def parse_condition(condition: str) -> tuple[str]:
             return column, operation, value
 
 
+def parse_aggregate_condition(condition: str) -> tuple[str]:
+    if '=' in condition:
+        column, aggregate_func = condition.split('=')
+
+        if aggregate_func in ('avg', 'min', 'max'):
+            return column, aggregate_func
+
+
 def filter_data(data: list[dict], condition: str) -> list[dict]:
     filtered = []
 
     if not condition:
-        for elem in data:
-            filtered.append(elem)
-
-        return filtered
+        return data
 
     column, operation, value = parse_condition(condition)
 
@@ -60,9 +65,38 @@ def filter_data(data: list[dict], condition: str) -> list[dict]:
     return filtered
 
 
+def aggregate_data(data: list[dict], condition: str) -> list[dict]:
+    if not condition:
+        return data
+
+    column, aggregate_func = parse_aggregate_condition(condition)
+
+    try:
+        values = [float(row[column]) for row in data]
+    except ValueError:
+        raise ValueError(f"Cannot perform aggregation on non-numeric column: {column}")
+
+    result = dict()
+
+    if len(values) == 0:
+        result[aggregate_func] = 'None'
+        return result
+
+    if aggregate_func == 'avg':
+        result['avg'] = sum(values) / len(values)
+    elif aggregate_func == 'min':
+        result['min'] = min(values)
+    else:
+        result['max'] = max(values)
+
+    return result
+
+
 def main():
     args = get_args()
     data = get_data(args.file)
+    data = filter_data(data, args.where)
+    data = aggregate_data(data, args.aggregate)
 
 
 if __name__ == '__main__':
