59c8c374117f7096c525eff98e473a6a3e75fb62
darkus079 <165654807+darkus079@users.noreply.github.com>
2025-06-25 20:59:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -26,10 +26,43 @@ def get_data(filename: str):
         return list(reader)
 
 
+def parse_condition(condition: str) -> tuple[str]:
+    operation_list = ['=', '>=', '<=', '>', '<']
+
+    for operation in operation_list:
+        if operation in condition:
+            column, value = condition.split(operation)
+            return column, operation, value
+
+
+def filter_data(data: list[dict], condition: str) -> list[dict]:
+    filtered = []
+
+    if not condition:
+        for elem in data:
+            filtered.append(elem)
+
+        return filtered
+
+    column, operation, value = parse_condition(condition)
+
+    for elem in data:
+        if isinstance(elem[column], int) and isinstance(value, int):
+            if eval(f"elem[{column}] {operation} {int(value)}"):
+                filtered.append(elem)
+        elif isinstance(elem[column], float) and isinstance(value, float):
+            if eval(f"elem[{column}] {operation} {float(value)}"):
+                filtered.append(elem)
+        else:
+            if eval(f"elem[{column}] {operation} {value}"):
+                filtered.append(elem)
+
+    return filtered
+
+
 def main():
     args = get_args()
     data = get_data(args.file)
-    print(data)
 
 
 if __name__ == '__main__':
